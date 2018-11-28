1409d5115bf19df7e1fc2421d7c020e37b45a39b
Carmine <kappa85t@hotmail.it>
2018-11-29 00:44:39 +0100

    init of basic react boilerplate project

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// JSX
+ReactDOM.render(<h1>Hello world<h1/>,document.getElementById('root'))
+
+
+ReactDOM.render(<ul>
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+</ul>, document.getElementById('root'));
