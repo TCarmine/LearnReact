06bd43ca2141d585c2885b9a790eb4c4c4748564
TCarmine <kappa85t@hotmail.it>
2019-07-23 18:08:52 +0200

    babel.config.js for react and that produce the bundler

--- babel.config.js
+++ babel.config.js
@@ -2,22 +2,22 @@
 
 const presets = [
    [ 
-        "@babel/preset-env", 
+        "@babel/preset-env",
         {
             useBuiltIns: "usage", // or "entry"
             corejs: 3,
-        },
-        {
             targets:
-            {
-                edge:"17",
-                firefox:"60",
-                chrome:"67",
-                safari:"11.1",
-            },
-        useBuiltIns: "usage",
-        },
-   ],  
+                {
+                    edge:"17",
+                    firefox:"60",
+                    chrome:"67",
+                    safari:"11.1",
+                },
+            useBuiltIns: "usage",
+        }
+        
+   ],
+   ["@babel/preset-react"]  
 ];
 
 module.exports = { presets };
