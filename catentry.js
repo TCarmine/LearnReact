427feb28e189af13f66835d1eae1d10bf9a99827
TCarmine <kappa85t@hotmail.it>
2019-07-26 09:17:36 +0200

    separete catcard app with config and entry point

---
+++ catentry.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import Todo from "./src/todoApp/components/Todo";
+import  MainCat from "./src/catcard/components/MainCat";
+
+
+
+ReactDOM.render(<MainCat />, document.getElementById("cat-app"));
---
+++ catweb.config.js
@@ -0,0 +1,29 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const config  = {
+
+  entry: "./catentry.js",
+  mode:'development',
+  output: {
+    filename: 'catbundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+//   check utility of this  
+//   plugins: [new HtmlWebpackPlugin({filename:'cat.html'})],  
+  module: {
+    rules: 
+    [
+      {
+         test: /\.(js|jsx)$/,
+         exclude: /node_modules/,
+         use: {
+            loader: "babel-loader",
+         }
+       }
+    ]
+  }
+};
+
+  module.exports = config;
