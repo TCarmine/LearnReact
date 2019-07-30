88a3190769647596d1b93d0ef964fd9d213a39f3
TCarmine <kappa85t@hotmail.it>
2019-07-30 09:03:35 +0200

    add state

--- src/todoApp/components/MainComponent.js
+++ src/todoApp/components/MainComponent.js
@@ -2,13 +2,24 @@ import React from "react"
 import TodoItem from "./TodoItem"
 import todosData from "../todosData"
 
-function MainComponent() {
-  const todoItems = todosData.map(item => <TodoItem key={item.id}  todoitem={item} />)
-  return (
-     <div className="todo-list">
-       {todoItems}
-     </div> 
-  )
+class MainComponent extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+       todos: todosData 
+    }
+  }
+    
+  render(){
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id}  todoitem={item} />)
+    return (
+      <div className="todo-list">
+        {todoItems}
+      </div> 
+   )
+  }
+  
 }
 
 export default MainComponent
